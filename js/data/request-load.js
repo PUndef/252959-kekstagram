4b541f1adc06952d652a7e97dca737a34c02a0e8
pzyryanov1995 <pzyryanov1995@gmail.com>
2018-02-23 12:57:12 +0700

    add modal error

--- js/data/request-load.js
+++ js/data/request-load.js
@@ -1,9 +1,15 @@
 'use strict';
 
 (function () {
+  var modal = document.querySelector('.modal-overlay');
+  var modalContent = document.querySelector('.modal-overlay-content');
 
-  var onError = function () {
-    // console.log('message');
+  var onError = function (message) {
+    modalContent.textContent = message;
+    modal.classList.remove('hidden');
+    setTimeout(function () {
+      modal.classList.add('hidden');
+    }, 2000);
   };
 
   window.data = {
