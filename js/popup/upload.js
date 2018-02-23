9339727995dcad470600249d4d5ac26513014d78
pzyryanov1995 <pzyryanov1995@gmail.com>
2018-02-23 13:12:14 +0700

    clear upload
    clear upload window after close

--- js/popup/upload.js
+++ js/popup/upload.js
@@ -5,10 +5,13 @@
   var defaulUploadResizeValue = 100;
   var defaultEffectLevel = 100;
   var defaultEffect = 'none';
+  var defaultEffectElement = document.querySelector('#upload-effect-none');
 
   var upload = document.querySelector('#upload-file');
   var uploadOverlay = document.querySelector('.upload-overlay');
   var uploadClose = document.querySelector('#upload-cancel');
+  var uploadFormHashtags = document.querySelector('.upload-form-hashtags');
+  var uploadFormDescription = document.querySelector('.upload-form-description');
 
   upload.addEventListener('change', function (evt) {
     evt.preventDefault();
@@ -26,6 +29,9 @@
       document.removeEventListener('keydown', window.popup.onUploadPopupEscPress);
       window.imageUploadResize.changeUploadImagesSize(defaulUploadResizeValue);
       window.imageChange.setNewEffectLevel(defaultEffectLevel, defaultEffect);
+      uploadFormHashtags.value = '';
+      uploadFormDescription.value = '';
+      defaultEffectElement.checked = true;
     }
   };
 
